e9fb5bd618ab76ac682a53cf96a7fe64076a1bfc
Pragati Dhage <pragatidhage971@gmail.com>
2024-03-18 10:48:55 +0530

    edited policy,contact,about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Layout from './../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <div className="row contactus">
+        <div className="col-md-6">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4" >
+          <p className="text-justify mt-2" >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            obcaecati temporibus error qui in veritatis omnis voluptatibus
+            aperiam, nobis eum minus reiciendis illum quia quos vero cupiditate,
+            expedita inventore esse veniam consequuntur commodi! Corrupti.
+          </p>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
