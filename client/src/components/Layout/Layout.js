53259920e7106149c90cb6a3bc6b2b1d1b8a10f6
Pragati Dhage <pragatidhage971@gmail.com>
2024-03-19 17:12:24 +0530

    performed SEO for Layout

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-const Layout = (props) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+       
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+       
+        <title>{title}</title>
+      </Helmet>
       <Header />
-      <main style={{minHeight: '80vh'}}>{props.children}</main>
+      <main style={{ minHeight: "80vh" }}>{children}</main>
       <Footer />
     </div>
   );
 };
+
+Layout.defaultProps = {
+  title : 'Ecommerce App-Shop Now',
+  description : 'mern stack project',
+  keywords : 'mern,react,node,mongodb',
+  author : 'TechSupport'
+}
 
 export default Layout;
