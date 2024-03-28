dd2d15c4a774f09c20f262b85ff8cb586c2e8bbb
Pragati Dhage <pragatidhage971@gmail.com>
2024-03-28 08:22:50 +0530

    forget password page added

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,10 +1,99 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React, { useState } from "react";
+import axios from "axios";
+import Layout from "../components/Layout/Layout";
+import { useNavigate } from "react-router-dom"
+import toast from "react-hot-toast"
+import "./../styles/AuthStyle.css";
+
 
 const ForgotPassword = () => {
+     
+  const [email, setEmail] = useState("");
+  const [newPassword, setnewPassword] = useState("");
+  const [answer, setAnswer] = useState("");
+ 
+
+  const navigate = useNavigate()
+
+   //hadle submit
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/forgot-password`,
+        {  email,answer,newPassword }
+      );
+      if( res && res.data.success){
+        toast.success( res.data && res.data.message)
+        
+        navigate( "/login")
+      }else {
+        toast.error(res.data.message)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   return (
-    <Layout>
-       <h1>Forgot Password</h1>
+    <Layout title={"Ecommerce App-ForgotPassword"}>
+      <div className="form-container">
+      
+        <form onSubmit={handleSubmit}>
+          <h3 className="title">RESET PASSWORD</h3>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail" className="form-label">
+              What is name of your first school?
+            </label>
+            <input
+              type="test"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword" className="form-label">
+              New Password
+            </label>
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setnewPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword"
+              placeholder="*********"
+              required
+            />
+          </div>
+
+         
+          <div className="mb-3">
+          <button type="submit" className="btn btn-primary">
+            RESET PASSWORD
+          </button>
+          </div>
+         
+         
+        </form>
+      </div>
     </Layout>
   )
 }
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address } = req.body;
+    const { name, email, password, phone, address,answer } = req.body;
     //validations
     if (!name) {
       return res.send({ message: "Name is Required" });
@@ -20,6 +20,9 @@ export const registerController = async (req, res) => {
     }
     if (!address) {
       return res.send({ message: "Address is Required" });
+    }
+    if (!answer) {
+      return res.send({ message: "Answer is Required" });
     }
 
     // check user
@@ -40,6 +43,7 @@ export const registerController = async (req, res) => {
       password: hashedPassword,
       phone,
       address,
+      answer
     });
     await user.save();
 
